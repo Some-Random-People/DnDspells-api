45c96172da7813f1803e740f82879fbc989e53d3
Master5111 <pawel9395@wp.pl>
2024-11-23 22:45:02 +0100

    Updating database connection config for better readability

--- database/database.go
+++ database/database.go
@@ -2,10 +2,12 @@ package database
 
 import (
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 	"log"
 	"os"
+
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 func ConnectToDatabase() *sql.DB { // This function only establishes sql.DB object, verifies it and returns it
@@ -14,7 +16,15 @@ func ConnectToDatabase() *sql.DB { // This function only establishes sql.DB obje
 		log.Fatal("Error loading .env file")
 	}
 	// Awful declaration but works
-	db, err := sql.Open("mysql", os.Getenv("DB_USER")+":"+os.Getenv("DB_PASS")+"@("+os.Getenv("DB_IP")+":"+os.Getenv("DB_PORT")+")/"+os.Getenv("DB_NAME"))
+	//db, err := sql.Open("mysql", os.Getenv("DB_USER")+":"+os.Getenv("DB_PASS")+"@("+os.Getenv("DB_IP")+":"+os.Getenv("DB_PORT")+")/"+os.Getenv("DB_NAME"))
+	cfg := mysql.Config{
+		User:   os.Getenv("DB_USER"),
+		Passwd: os.Getenv("DB_PASS"),
+		Net:    "tcp",
+		Addr:   os.Getenv("DB_IP") + ":" + os.Getenv("DB_PORT"),
+		DBName: os.Getenv("DB_NAME"),
+	}
+	db, err := sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,6 +33,7 @@ func ConnectToDatabase() *sql.DB { // This function only establishes sql.DB obje
 	if err != nil {
 		log.Fatal("No connection to database")
 	}
+	log.Println("Connected to database")
 
 	return db // Return pointer to databse for further connections
 }
